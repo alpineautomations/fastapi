1f64cef8b2ac00faa243d590cbc3cf2ef4db4c03
Neil <neilantony@Neils-MacBook-Pro.local>
2026-09-07 12:57:36 +0100

    updated github to .github

--- test/test_posts.py
+++ test/test_posts.py
@@ -95,3 +95,13 @@ def test_update_other_user_post(authorized_client, test_user, test_posts):
     }
     res = authorized_client.put(f"/posts/{test_posts[3].id}", json=data)
     assert res.status_code == 403
+
+def test_unauthorized_user_update_post(client, test_user, test_posts):
+    res = client.put(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
+
+def test_update_post_non_exist(authorized_client, test_user, test_posts):
+    data = {"title": "updated title", "content": "updated content"}
+    res = authorized_client.put("/posts/800000", json=data)
+    assert res.status_code == 404
+
